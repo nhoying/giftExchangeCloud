d2db4dfcd32b03626b33c1d81d6dc44df150478c
Nicole Hoying <nhoying@gmail.com>
2024-08-05 14:56:16 -0400

    add gitignore

--- api/GiftExchangeApi/GiftExchange.Data/GiftExchangeDb/GiftExchangeRepository.cs
+++ api/GiftExchangeApi/GiftExchange.Data/GiftExchangeDb/GiftExchangeRepository.cs
@@ -1,8 +1,14 @@
-﻿namespace GiftExchange.Data.GiftExchangeDb;
+﻿using GiftExchange.Data.GiftExchangeDb.Schema;
+
+namespace GiftExchange.Data.GiftExchangeDb;
 
 public interface IGiftExchangeRepository
 {
-    
+    // Exchange
+    Task<Exchange> GetExchange(Guid exchangeIdentifier);
+    Task AddExchange(Exchange exchange);
+    Task UpdateExchange(Exchange exchange);
+    Task RemoveExchange(Guid exchangeIdentifier);
 }
 
 public class GiftExchangeRepository
